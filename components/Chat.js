967e2fc24f8f5f537f9c866b26330591389d6150
obitown <92471740+obitown@users.noreply.github.com>
2022-04-19 17:50:00 -0700

    Fix Navigation

--- components/Chat.js
+++ components/Chat.js
@@ -1,17 +1,25 @@
 import React from "react";
 import { View, Text } from 'react-native'
 
+
 export default class Chat extends React.Component {
+
+
     render() {
+        let name = this.props.route.params.name;
+        this.props.navigation.setOptions({ title: name });
+
+        const { bgColor } = this.props.route.params
         return (
             <View
                 style={{
                     flex: 1,
                     justifyContent: 'center',
-                    alignItems: 'center'
+                    alignItems: 'center',
+                    backgroundColor: bgColor
                 }}
             >
-                <Text>Hello Screen2!</Text>
+                <Text></Text>
             </View>
         )
     }
--- components/Start.js
+++ components/Start.js
@@ -1,24 +1,129 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, StyleSheet, TouchableOpacity, } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+
 
 export default class Start extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: '',
+        }
+    }
+
+    changeBgColor = (newColor) => {
+        this.setState({ bgColor: newColor });
+    };
+
+    colors = {
+        dark: '#090C08',
+        purple: '#474056',
+        blue: '#8A95A5',
+        green: '#B9C6AE'
+    };
+
     render() {
-        // let name = this.props.route.params.name;
-        // this.props.navigation.setOptions({ title: name })
+        this.props.navigation.setOptions({ title: 'Welcome to Obi-Chat' });
         return (
-            <View
-                style={{
-                    flex: 1,
-                    justifyContent: 'center',
-                    alignItems: 'center'
-                }}
-            >
-                <Text>Hello Chat Screen</Text>
+            <View style={styles.container}>
+                <View style={styles.nameInput}>
+                    <TextInput
+                        style={styles.input}
+                        onChangeText={(text) => this.setState({ name: text })}
+                        value={this.state.name}
+                        placeholder="Choose a name"
+                    />
+                </View>
+
+
+                <View>
+                    <Text style={styles.chooseColor}> Choose Background Color: </Text>
+                </View>
+
+                <View style={styles.colorArray}>
+                    <TouchableOpacity
+                        style={styles.color1}
+                        onPress={() => this.changeBgColor(this.colors.dark)}>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.color2}
+                        onPress={() => this.changeBgColor(this.colors.purple)}>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.color3}
+                        onPress={() => this.changeBgColor(this.colors.blue)}>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.color4}
+                        onPress={() => this.changeBgColor(this.colors.green)}>
+                    </TouchableOpacity>
+                </View>
                 <Button
-                    title='Go to Screen 2'
-                    onPress={() => this.props.navigation.navigate('Chat')}
+                    title='Start Chatting'
+                    onPress={() => this.props.navigation.navigate('Chat', {
+                        name: this.state.name,
+                        bgColor: this.state.bgColor
+                    })}
                 />
+
             </View>
         )
     }
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    nameInput: {
+        borderColor: 'grey',
+        borderWidth: 3,
+        borderRadius: 3,
+        height: 60,
+        width: '50%',
+        margin: 10
+    },
+    input: {
+        fontSize: 17,
+        paddingTop: 10,
+        textAlign: 'center'
+    },
+    chooseColor: {
+        fontWeight: '900'
+    },
+    colorArray: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+
+    },
+    color1: {
+        backgroundColor: '#090C08',
+        width: 50,
+        height: 50,
+        borderRadius: 25
+    },
+
+    color2: {
+        backgroundColor: '#474056',
+        width: 50,
+        height: 50,
+        borderRadius: 25
+    },
+
+    color3: {
+        backgroundColor: '#8A95A5',
+        width: 50,
+        height: 50,
+        borderRadius: 25
+    },
+
+    color4: {
+        backgroundColor: '#B9C6AE',
+        width: 50,
+        height: 50,
+        borderRadius: 25
+    },
+
+})
