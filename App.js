3328c930f5a313fba1dca83ca939160b2a44b1ef
obitown <92471740+obitown@users.noreply.github.com>
2022-04-21 14:16:20 -0700

    add chat box

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import { View } from 'react-native';
 
 //import Screens
 import Start from './components/Start';
--- components/Chat.js
+++ components/Chat.js
@@ -1,9 +1,41 @@
 import React from "react";
-import { View, Text } from 'react-native'
+import { View, Platform, KeyboardAvoidingView } from 'react-native'
+
+//import GiftedChat
+import { GiftedChat } from "react-native-gifted-chat";
 
 
 export default class Chat extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            messages: [],
+        }
+    }
 
+    componentDidMount() {
+        this.setState({
+            messages: [
+                {
+                    _id: 1,
+                    text: 'Hello developer',
+                    createdAt: new Date(),
+                    user: {
+                        _id: 2,
+                        name: 'React Native',
+                        avatar: 'https://placeimg.com/140/140/any'
+                    },
+                },
+            ],
+        })
+    }
+
+    //function to send messages
+    onSend(messages = []) {
+        this.setState(previousState => ({
+            messages: GiftedChat.append(previousState.messages, messages),
+        }))
+    }
 
     render() {
         let name = this.props.route.params.name;
@@ -19,7 +51,16 @@ export default class Chat extends React.Component {
                     backgroundColor: bgColor
                 }}
             >
-                <Text></Text>
+                <View style={{ flex: 1 }}>
+                    <GiftedChat
+                        messages={this.state.messages}
+                        onSend={messages => this.onSend(messages)}
+                        user={{
+                            _id: 1,
+                        }}
+                    />
+                    {/* {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null} */}
+                </View>
             </View>
         )
     }
