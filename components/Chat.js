c46cefab4672b3ca4b2504bb0cbc5a920ebac610
obitown <92471740+obitown@users.noreply.github.com>
2022-04-27 18:33:50 -0700

    somewhat fix

--- components/Chat.js
+++ components/Chat.js
@@ -18,6 +18,7 @@ export default class Chat extends React.Component {
             user: {
                 _id: '',
                 name: '',
+                avatar: '',
 
             },
             isConnected: false,
@@ -39,13 +40,23 @@ export default class Chat extends React.Component {
             firebase.initializeApp(firebaseConfig);
         }
 
-        this.referenceMessages = firebase.firestore().collection('messages');
+        this.referenceChatMessages = firebase.firestore().collection('messages');
 
     }
 
     componentDidMount() {
         this.setState({
             messages: [
+                // {
+                //     _id: 1,
+                //     text: 'Hello developer',
+                //     createdAt: new Date(),
+                //     user: {
+                //         _id: 2,
+                //         name: 'React Native',
+                //         avatar: 'https://placeimg.com/140/140/any'
+                //     },
+                // },
                 {
                     _id: 2,
                     text: `Welcome to Obi-Chat!`,
@@ -56,18 +67,33 @@ export default class Chat extends React.Component {
         })
 
         this.referenceChatMessages = firebase.firestore().collection('messages');
+        this.unsubscribe = this.referenceChatMessages.onSnapshot(this.onCollectionUpdate);
 
     }
 
     componentWillUnmount() {
-        // this.unsubscribe();
+        this.unsubscribe();
     }
 
     //function to send messages
     onSend(messages = []) {
         this.setState(previousState => ({
             messages: GiftedChat.append(previousState.messages, messages),
-        }))
+        }), () => {
+            this.addMessage();
+        })
+    }
+
+    addMessage() {
+        const message = this.state.messages[0];
+
+        this.referenceChatMessages.add({
+            uid: this.state.uid,
+            _id: message._id,
+            text: message.text || '',
+            createdAt: message.createdAt,
+            user: this.state.user,
+        });
     }
 
     onCollectionUpdate = (querySnapshot) => {
@@ -78,7 +104,7 @@ export default class Chat extends React.Component {
             let data = doc.data();
             messages.push({
                 _id: data._id,
-                test: data.text,
+                text: data.text,
                 createdAt: data.createdAt.toDate(),
                 user: data.user,
             });
@@ -98,7 +124,7 @@ export default class Chat extends React.Component {
                         backgroundColor: '#0008ff'
                     },
                     left: {
-                        backgroundColor: '#fff'
+                        backgroundColor: '#9e9e9e'
                     }
                 }}
             />
@@ -107,7 +133,7 @@ export default class Chat extends React.Component {
 
     render() {
         let name = this.props.route.params.name;
-        // this.props.navigation.setOptions({ title: name });
+        this.props.navigation.setOptions({ title: name });
 
         const { bgColor } = this.props.route.params
         return (
@@ -125,8 +151,7 @@ export default class Chat extends React.Component {
                         onSend={messages => this.onSend(messages)}
                         user={{
                             _id: 1,
-                            name: this.state.name,
-
+                            user: this.state.name,
                         }}
                     />
                     {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
